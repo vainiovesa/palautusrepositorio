5074c7570725095f58f7a378543d58e565449fc4
vainiovesa <vesa.vainio@helsinki.fi>
2025-10-29 18:12:06 +0200

    Complete 15

--- viikko1/nhl-statistics/src/statistics_service.py
+++ viikko1/nhl-statistics/src/statistics_service.py
@@ -3,8 +3,8 @@ from player_reader import PlayerReader
 
 
 class StatisticsService:
-    def __init__(self):
-        reader = PlayerReader()
+    def __init__(self, playerreader):
+        reader = playerreader
 
         self._players = reader.get_players()
 
