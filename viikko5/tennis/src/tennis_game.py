73554c936aed74e8d13386f8a595f5bc035f484e
vainiovesa <vesa.vainio@helsinki.fi>
2025-11-26 19:04:20 +0200

    Refactor

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -9,41 +9,43 @@ class TennisGame:
         self.player_scores[player_name] += 1
 
     def get_score(self):
-        score = ""
-        temp_score = 0
-
+        limit = 3
         player1_score = self.player_scores[self.player1_name]
         player2_score = self.player_scores[self.player2_name]
 
         if player1_score == player2_score:
-            scores = ["Love-All", "Fifteen-All", "Thirty-All", "Deuce"]
-            if player1_score < 4:
-                score = scores[player1_score]
-            else:
-                score = scores[-1]
-
-        elif player1_score >= 4 or player2_score >= 4:
-            minus_result = player1_score - player2_score
-
-            if minus_result == 1:
-                score = "Advantage player1"
-            elif minus_result == -1:
-                score = "Advantage player2"
-            elif minus_result >= 2:
-                score = "Win for player1"
-            else:
-                score = "Win for player2"
+            score_str = self._scores_equal(player1_score)
+        elif player1_score > limit or player2_score > limit:
+            score_str = self._players_score_over_limit(player1_score, player2_score)
         else:
-            scores = ["Love", "Fifteen", "Thirty", "Forty"]
-            for i in range(1, 3):
-                if i == 1:
-                    temp_score = player1_score
-                else:
-                    score = score + "-"
-                    temp_score = player2_score
+            score_str = self._scores_under_limit(player1_score, player2_score)
 
-                score = score + scores[temp_score]
+        return score_str
 
-        return score
+    def _scores_equal(self, player_score):
+        scores = ["Love-All", "Fifteen-All", "Thirty-All", "Deuce"]
+        if player_score < 4:
+            score_str = scores[player_score]
+        else:
+            score_str = scores[-1]
+        return score_str
 
+    def _players_score_over_limit(self, player1_score, player2_score):
+        diff = player1_score - player2_score
 
+        if diff == 1:
+            score_str = "Advantage player1"
+        elif diff == -1:
+            score_str = "Advantage player2"
+        elif diff >= 2:
+            score_str = "Win for player1"
+        else:
+            score_str = "Win for player2"
+        
+        return score_str
+
+    def _scores_under_limit(self, player1_score, player2_score):
+        scores = ["Love", "Fifteen", "Thirty", "Forty"]
+        score_str = scores[player1_score] + "-" + scores[player2_score]
+
+        return score_str
