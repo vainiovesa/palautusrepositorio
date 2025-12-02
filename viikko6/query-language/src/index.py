a4188966f4244470c4b1bcee36bcb17fab6a567f
vainiovesa <vesa.vainio@helsinki.fi>
2025-12-02 18:29:03 +0200

    Complete 3

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,20 +1,28 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn
+from matchers import And, HasAtLeast, PlaysIn, All, Not, HasFewerThan
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players.txt"
     reader = PlayerReader(url)
     stats = Statistics(reader)
 
+    # matcher = And(
+    #     HasAtLeast(5, "goals"),
+    #     HasAtLeast(20, "assists"),
+    #     PlaysIn("PHI")
+    # )
+
     matcher = And(
-        HasAtLeast(5, "goals"),
-        HasAtLeast(20, "assists"),
-        PlaysIn("PHI")
+        HasFewerThan(2, "goals"),
+        PlaysIn("NYR")
     )
 
     for player in stats.matches(matcher):
         print(player)
+    
+    filtered_with_all = stats.matches(All())
+    print(len(filtered_with_all))
 
 
 if __name__ == "__main__":
--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -27,3 +27,27 @@ class HasAtLeast:
         player_value = getattr(player, self._attr)
 
         return player_value >= self._value
+
+
+class All:
+    def test(self, player):
+        return True
+
+
+class Not:
+    def __init__(self, matcher):
+        self.matcher = matcher
+
+    def test(self, player):
+        return False if self.matcher.test(player) else True
+
+
+class HasFewerThan:
+    def __init__(self, value, attr):
+        self._value = value
+        self._attr = attr
+
+    def test(self, player):
+        player_value = getattr(player, self._attr)
+
+        return player_value < self._value
