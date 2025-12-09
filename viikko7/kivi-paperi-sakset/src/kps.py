755faf060272143f40801704804e10b4e36b5307
vainiovesa <vesa.vainio@helsinki.fi>
2025-12-09 19:16:55 +0200

    Use template method in kps.py

--- viikko7/kivi-paperi-sakset/src/kps.py
+++ viikko7/kivi-paperi-sakset/src/kps.py
@@ -2,75 +2,59 @@ from tuomari import Tuomari
 from tekoaly import Tekoaly, TekoalyParannettu
 
 
-class KPSPelaajaVsPelaaja:
+class KiviPaperiSakset:
     def pelaa(self):
         tuomari = Tuomari()
 
-        ekan_siirto = input("Ensimmäisen pelaajan siirto: ")
+        ekan_siirto = self._ensimmaisen_siirto()
+        tokan_siirto = self._toisen_siirto(ekan_siirto)
+
+        while self._onko_ok_siirto(ekan_siirto) and self._onko_ok_siirto(tokan_siirto):
+            tuomari.kirjaa_siirto(ekan_siirto, tokan_siirto)
+            print(tuomari)
+
+            ekan_siirto = self._ensimmaisen_siirto()
+            tokan_siirto = self._toisen_siirto(ekan_siirto)
+
+        print("Kiitos!")
+        print(tuomari)
+
+    def _ensimmaisen_siirto(self):
+        return input("Ensimmäisen pelaajan siirto: ")
+
+    def _toisen_siirto(self, ensimmaisen_siirto):
+        raise Exception("Tämä metodi pitää korvata aliluokassa")
+
+    def _onko_ok_siirto(self, siirto):
+        return siirto == "k" or siirto == "p" or siirto == "s"
+
+
+
+class KPSPelaajaVsPelaaja(KiviPaperiSakset):
+    def _toisen_siirto(self, ensimmaisen_siirto):
         tokan_siirto = input("Toisen pelaajan siirto: ")
 
-        while self._onko_ok_siirto(ekan_siirto) and self._onko_ok_siirto(tokan_siirto):
-            tuomari.kirjaa_siirto(ekan_siirto, tokan_siirto)
-            print(tuomari)
-
-            ekan_siirto = input("Ensimmäisen pelaajan siirto: ")
-            tokan_siirto = input("Toisen pelaajan siirto: ")
-
-        print("Kiitos!")
-        print(tuomari)
-
-    def _onko_ok_siirto(self, siirto):
-        return siirto == "k" or siirto == "p" or siirto == "s"
+        return tokan_siirto
 
 
-class KPSTekoaly:
-    def pelaa(self):
-        tuomari = Tuomari()
-        tekoaly = Tekoaly()
+class KPSTekoaly(KiviPaperiSakset):
+    def __init__(self):
+        self.tekoaly = Tekoaly()
 
-        ekan_siirto = input("Ensimmäisen pelaajan siirto: ")
-        tokan_siirto = tekoaly.anna_siirto()
-
+    def _toisen_siirto(self, ensimmaisen_siirto):
+        tokan_siirto = self.tekoaly.anna_siirto()
         print(f"Tietokone valitsi: {tokan_siirto}")
 
-        while self._onko_ok_siirto(ekan_siirto) and self._onko_ok_siirto(tokan_siirto):
-            tuomari.kirjaa_siirto(ekan_siirto, tokan_siirto)
-            print(tuomari)
-
-            ekan_siirto = input("Ensimmäisen pelaajan siirto: ")
-            tokan_siirto = tekoaly.anna_siirto()
-
-            print(f"Tietokone valitsi: {tokan_siirto}")
-
-        print("Kiitos!")
-        print(tuomari)
-
-    def _onko_ok_siirto(self, siirto):
-        return siirto == "k" or siirto == "p" or siirto == "s"
+        return tokan_siirto
 
 
-class KPSParempiTekoaly:
-    def pelaa(self):
-        tuomari = Tuomari()
-        tekoaly = TekoalyParannettu(10)
+class KPSParempiTekoaly(KiviPaperiSakset):
+    def __init__(self):
+        self.tekoaly = TekoalyParannettu(10)
 
-        ekan_siirto = input("Ensimmäisen pelaajan siirto: ")
-        tokan_siirto = tekoaly.anna_siirto()
-
+    def _toisen_siirto(self, ensimmaisen_siirto):
+        tokan_siirto = self.tekoaly.anna_siirto()
         print(f"Tietokone valitsi: {tokan_siirto}")
+        self.tekoaly.aseta_siirto(ensimmaisen_siirto)
 
-        while self._onko_ok_siirto(ekan_siirto) and self._onko_ok_siirto(tokan_siirto):
-            tuomari.kirjaa_siirto(ekan_siirto, tokan_siirto)
-            print(tuomari)
-
-            ekan_siirto = input("Ensimmäisen pelaajan siirto: ")
-            tokan_siirto = tekoaly.anna_siirto()
-
-            print(f"Tietokone valitsi: {tokan_siirto}")
-            tekoaly.aseta_siirto(ekan_siirto)
-
-        print("Kiitos!")
-        print(tuomari)
-
-    def _onko_ok_siirto(self, siirto):
-        return siirto == "k" or siirto == "p" or siirto == "s"
+        return tokan_siirto
