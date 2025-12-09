8ea065be2e93f703d1df05f9bce11643a2232663
vainiovesa <vesa.vainio@helsinki.fi>
2025-12-09 18:52:25 +0200

    Untangle file spaghetti

--- viikko7/kivi-paperi-sakset/src/index.py
+++ viikko7/kivi-paperi-sakset/src/index.py
@@ -1,7 +1,4 @@
-from kps_pelaaja_vs_pelaaja import KPSPelaajaVsPelaaja
-from kps_tekoaly import KPSTekoaly
-from kps_parempi_tekoaly import KPSParempiTekoaly
-
+from kps import KPSPelaajaVsPelaaja, KPSTekoaly, KPSParempiTekoaly
 
 def main():
     while True:
